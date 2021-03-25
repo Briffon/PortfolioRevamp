376c9311bad7464f265a9e5c304d5102b8932903
britton smith <ashyoda@gmail.com>
2021-03-25 15:10:20 -0400

    made pagination for carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -10,16 +10,44 @@ function Carousel(props) {
 
     const switchPage=(e,page)=>{
         e.preventDefault();
-        console.log(page);
+        setPage(page);
+    }
+
+    const next =(e)=>{
+        e.preventDefault();
+        let next=page+1;
+        if(props.items[next]){
+            setPage(next);
+            if(document.getElementsByClassName('disable')[0]){
+                document.getElementsByClassName('disable')[0].classList.remove('disable');
+            }
+
+            if(!props.items[next+1]){
+                document.getElementsByClassName('carousel-container__pagination-next')[0].classList.add('disable');
+            }
+        }
+    }
+
+    const previous =(e)=>{
+        e.preventDefault();
+        let previous = page-1;
+
+        if(props.items[previous]){
+            setPage(previous);
+            if(document.getElementsByClassName('disable')[0]){
+                document.getElementsByClassName('disable')[0].classList.remove('disable');
+            }
+            if(!props.items[previous-1]){
+                document.getElementsByClassName('carousel-container__pagination-previous')[0].classList.add('disable');
+            }
+        }
     }
 
     return (
         <div style={{width:props.width, height:props.height}} className={`${props.className ? props.className :null} carousel-container`}>
-            {console.log(props.items[page].title)}
-            {page ? 
-            <div className='carousel-container__item'>
+            <div className="carousel-container__item">
                 <p>{props.items[page].title}</p>
-            </div>:null}
+            </div>
             {/* {props.items ? props.items.map((item,id)=>{
                 if(id!==0){
                     return(
@@ -42,6 +70,11 @@ function Carousel(props) {
                         <Button onClick={e=>switchPage(e,id)} key={id} className="carousel-container__buttons-button" content={button.title}/>
                     )
                 }):null}
+            </div>
+
+            <div carousel-container__pagination>
+                <button onClick={e=>previous(e)} className=" disable carousel-container__pagination-previous">{`<`}</button>
+                <button onClick={e=>next(e)} className="carousel-container__pagination-next">{`>`}</button>
             </div>
         </div>
     )
